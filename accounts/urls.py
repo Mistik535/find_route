82982e620847c2dd3b6455d76a1e86a1ebdd024d
Mistik <mr.chadway@gmail.com>
2021-06-23 21:55:25 +0300

    fix src

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from src.accounts.views import login_view, logout_view, registration_view
+from accounts.views import login_view, logout_view, registration_view
 
 urlpatterns = [
     path("login/", login_view, name="login"),
